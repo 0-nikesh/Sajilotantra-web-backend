0dab0ee74fa2dfa76be74ab32cf27331bcb669fd
nikesh <bhandarinikesh93@gmail.com>
2024-12-22 12:54:22 +0545

    model-4-feedback schema created

---
+++ model/Feedback.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const FeedbackSchema = new mongoose.Schema({
+  Category: Number,
+  suggestion: Number,
+  file: String,
+  feedback: String,
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+});
+
+module.exports = mongoose.model("Feedback", FeedbackSchema);
