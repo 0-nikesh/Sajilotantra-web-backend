ccd9e8994f13e19eabbfe6dc9ecd7297b9e4ceb9
nikesh <bhandarinikesh93@gmail.com>
2024-12-22 13:03:21 +0545

    controller-5- logic of feedback

---
+++ controller/FeedbackController.js
@@ -0,0 +1,13 @@
+const Feedback = require("../model/Feedback");
+
+const submitFeedback = async (req, res) => {
+  const feedback = await Feedback.create(req.body);
+  res.status(201).json(feedback);
+};
+
+const getAllFeedback = async (req, res) => {
+  const feedback = await Feedback.find();
+  res.json(feedback);
+}
+
+module.exports = { submitFeedback, getAllFeedback };
