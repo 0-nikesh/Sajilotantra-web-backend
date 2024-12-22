428cec7f1e73dd1e5da93f02516c51046df3d1de
nikesh <bhandarinikesh93@gmail.com>
2024-12-22 21:16:59 +0545

    User-6-admin only routing added

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const { protect, admin } = require("../middleware/authMiddleware");
+
 const {
   registerUser,
   loginUser,
   getAllUsers,
   getUserById,
   deleteUser,
+  getProfile,
 } = require("../controller/UserController");
-const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
@@ -15,8 +17,9 @@ router.post("/register", registerUser); // User registration
 router.post("/login", loginUser);       // User login
 
 // Admin-only actions (e.g., get all users, delete users)
-router.get("/", protect, getAllUsers);       // Get all users (protected)
-router.get("/:id", protect, getUserById);   // Get a user by ID (protected)
-router.delete("/:id", protect, deleteUser); // Delete a user (protected)
+router.get("/all", protect, admin, getAllUsers);       // Get all users (protected)
+router.get("/:id", protect, admin, getUserById);   // Get a user by ID (protected)
+router.delete("/:id", protect, admin, deleteUser); // Delete a user (protected)
+router.get("/profile", protect, getProfile);
 
 module.exports = router;
