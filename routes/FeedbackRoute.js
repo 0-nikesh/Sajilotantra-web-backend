1d122cb80f1c73e83b3557ad01a2cbd2f849815f
nikesh <bhandarinikesh93@gmail.com>
2024-12-22 21:13:33 +0545

    Feedback-1- routing for post and getall(admin only)

--- routes/FeedbackRoute.js
+++ routes/FeedbackRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { submitFeedback, getAllFeedback } = require("../controller/FeedbackController");
+const { admin, protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/", submitFeedback);
-router.get("/getall", getAllFeedback)
+router.post("/", protect, submitFeedback);
+router.get("/getall", protect, admin, getAllFeedback)
 
 module.exports = router;
