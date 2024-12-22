603f8c3256cd222fdd8a8b9a47c8d9c99d4a1582
nikesh <bhandarinikesh93@gmail.com>
2024-12-22 20:56:05 +0545

    Post-5-routing of post(delete, getall(admin), create)

--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { createPost, getAllPosts, getPostById, deletePost } = require("../controller/PostController");
-const { protect } = require("../middleware/authMiddleware");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", protect, createPost);
-router.get("/", getAllPosts);
+router.get("/all", protect, admin, getAllPosts);
 router.get("/:id", getPostById);
-router.delete("/:id", protect, deletePost); //deleted 
+router.delete("/:id", protect, deletePost);
 
 module.exports = router;
