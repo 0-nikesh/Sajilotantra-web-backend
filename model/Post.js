95bfcb13fd9b52445049d89302cb7e4d36fa0759
nikesh <bhandarinikesh93@gmail.com>
2024-12-27 20:52:21 +0545

    Post-10-schema changed to track liked by

--- model/Post.js
+++ model/Post.js
@@ -7,7 +7,8 @@ const PostSchema = new mongoose.Schema({
   created_at: { type: Date, default: Date.now },
   like_count: { type: Number, default: 0 },
   comment: { type: Number, default: 0 },
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  liked_by: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }] // Track users who liked the post
 });
 
 module.exports = mongoose.model("Post", PostSchema);
