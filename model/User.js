6e6bf1074040862a3223c36c9e22f24cbb70856b
nikesh <bhandarinikesh93@gmail.com>
2025-01-23 21:29:49 +0545

    user-7- OTP added

--- model/User.js
+++ model/User.js
@@ -5,7 +5,9 @@ const UserSchema = new mongoose.Schema({
   lname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false }
+  isAdmin: { type: Boolean, default: false },
+  otp: { type: String }, // Temporary OTP for email verification
+  otpExpiresAt: { type: Date }, // OTP expiration time
 });
 
 const User = mongoose.model('User', UserSchema);
