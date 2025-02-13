52f8d1f85d0d887abc9a8a53b04deb3bc8b419ec
nikesh <bhandarinikesh93@gmail.com>
2025-02-14 00:05:05 +0545

    cleaned

--- model/GovernmentProfile.js
+++ model/GovernmentProfile.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const GovernmentProfileSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, trim: true },
   thumbnail: { type: String },
   description: { type: String, required: true },
   address: { type: String, required: true },
@@ -17,8 +17,11 @@ const GovernmentProfileSchema = new mongoose.Schema({
   ],
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
 });
 
+GovernmentProfileSchema.index({ latitude: 1, longitude: 1 }, { type: "2dsphere" });
+
 const GovernmentProfile = mongoose.model("GovernmentProfile", GovernmentProfileSchema);
-module.exports = GovernmentProfile;
+export default GovernmentProfile;
+
