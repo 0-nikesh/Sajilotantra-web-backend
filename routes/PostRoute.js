e19fb79a9203b16d38700c86ca0a5c1c7c3a325f
nikesh <bhandarinikesh93@gmail.com>
2025-03-02 17:10:10 +0545

    Post-15- added getall post

--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -7,16 +7,20 @@ import {
     getCommentsForPost,
     getPostById,
     likePost,
+    getUserPosts
 } from '../controller/PostController.js';
-import { admin, protect } from '../middleware/authMiddleware.js'; // Change 'require' to 'import'
+import { protect } from '../middleware/authMiddleware.js'; // Change 'require' to 'import'
 const router = express.Router();
 
 router.post("/", protect, createPost);
-router.get("/all", protect, admin, getAllPosts);
+router.get("/all", protect, getAllPosts);
 router.get("/:id", getPostById);
 router.delete("/:id", protect, deletePost);
 router.post("/:id/like", protect, likePost);
 router.post("/:id/comment", protect, addComment);
 router.get("/:id/comments", protect, getCommentsForPost); // New route for fetching comments
+
+
+router.get("/user/:userId", protect, getUserPosts); // Get posts for a specific user
 
 export default router;
