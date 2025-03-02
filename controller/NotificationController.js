c889da4a69027a24d8705bb47fa8498da97226cf
nikesh <bhandarinikesh93@gmail.com>
2025-03-02 17:10:52 +0545

    fetching through logged in user

--- controller/NotificationController.js
+++ controller/NotificationController.js
@@ -1,14 +1,16 @@
-import Notification from ("../model/Notification.js");
+import Notification from "../model/Notification.js";
 
 // Fetch all notifications
+// Fetch notifications for the logged-in user
 const getNotifications = async (req, res) => {
   try {
-    const notifications = await Notification.find({});
+    const notifications = await Notification.find({ user_id: req.user._id }).sort({ date_posted: -1 });
     res.json(notifications);
   } catch (error) {
     res.status(500).json({ message: "Error fetching notifications" });
   }
 };
+
 
 // Create a notification (Admin-only)
 const createNotification = async (req, res) => {
